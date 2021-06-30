227413815b2576262d48ebad1931bdc3a2d85553
Danilo Ribeiro <danilorrg@gmail.com>
2021-06-29 22:24:22 -0300

    Adicionado configuração do Swagger

--- src/main/java/br/com/danilo/resources/ConsultaServidorResource.java
+++ src/main/java/br/com/danilo/resources/ConsultaServidorResource.java
@@ -10,21 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.danilo.dto.RootDTO;
 import br.com.danilo.service.ConsultaServidorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("servidores")
+@Api("Servidores Tremulous")
 public class ConsultaServidorResource {
 	
 	@Autowired
 	private ConsultaServidorService service;
 	
 	@GetMapping("jogadores-online")
+	@ApiOperation(value = "Retorna todos os servidores com jogadores online")
 	private List<RootDTO> consultarJogadoresOnline(){
 		return service.consultarComJogadoresOnline();
 	}
 	
 	@GetMapping
+	@ApiOperation(value = "Retorna todos os servidores")
 	private List<RootDTO> consultarTodos(){
 		return service.consultarTodosServidores();
 	}
